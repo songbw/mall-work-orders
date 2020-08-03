7a0010f3c55739d540fcc86ca5bb0bda26de1efe
song bingwei <bingwei.song@weesharing.com>
2020-08-03 18:12:04 +0800

    workorder

--- src/main/java/com/fengchao/workorders/controller/ThirdWorkOrderController.java
+++ src/main/java/com/fengchao/workorders/controller/ThirdWorkOrderController.java
@@ -5,21 +5,24 @@ import com.fengchao.workorders.service.impl.WorkOrderServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 订单列表
  */
 @RestController
-@RequestMapping(value = "/third/workorder", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(value = "/third", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Slf4j
 public class ThirdWorkOrderController {
 
     @Autowired
     private WorkOrderServiceImpl service;
 
-    @GetMapping("/{orderId}")
-    private OperaResponse getAll(@PathVariable("orderId") Integer orderId) {
+    @GetMapping("workorder")
+    private OperaResponse getAll(@RequestParam("orderId") Integer orderId) {
         OperaResponse response = new OperaResponse() ;
         response.setData(service.selectWorkOrderByOrderId(orderId));
         return response;
