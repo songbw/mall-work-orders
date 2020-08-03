320b1bf1799cd994de14ba075ba182c08536e5cc
song bingwei <bingwei.song@weesharing.com>
2020-08-03 17:57:04 +0800

    error

--- src/main/java/com/fengchao/workorders/controller/MyErrorController.java
+++ src/main/java/com/fengchao/workorders/controller/MyErrorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+//@ControllerAdvice
 @ResponseBody
 public class MyErrorController {
 
