b76c94df9a5dab2b93e91edad99dfa69fea185dc
song bingwei <bingwei.song@weesharing.com>
2020-08-03 19:21:35 +0800

    error

--- src/main/java/com/fengchao/workorders/controller/MyErrorController.java
+++ src/main/java/com/fengchao/workorders/controller/MyErrorController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+//@ControllerAdvice
 @ResponseBody
 public class MyErrorController {
 
