4389caa5acb00b59a9381edeccaa44d7baebc028
song bingwei <bingwei.song@weesharing.com>
2020-08-03 17:25:02 +0800

    WorkOrderServiceImpl

--- src/main/java/com/fengchao/workorders/controller/ThirdWorkOrderController.java
+++ src/main/java/com/fengchao/workorders/controller/ThirdWorkOrderController.java
@@ -1,7 +1,7 @@
 package com.fengchao.workorders.controller;
 
 import com.fengchao.workorders.bean.OperaResponse;
-import com.fengchao.workorders.service.IWorkOrderService;
+import com.fengchao.workorders.service.impl.WorkOrderServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ThirdWorkOrderController {
 
     @Autowired
-    private IWorkOrderService service;
+    private WorkOrderServiceImpl service;
 
     @GetMapping
     private OperaResponse getAll(Integer orderId) {
