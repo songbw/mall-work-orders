3f62f5ebb0fa97afe8e234496f7b88f050d87340
song bingwei <bingwei.song@weesharing.com>
2020-08-03 17:47:03 +0800

    param

--- src/main/java/com/fengchao/workorders/controller/ThirdWorkOrderController.java
+++ src/main/java/com/fengchao/workorders/controller/ThirdWorkOrderController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,7 +22,7 @@ public class ThirdWorkOrderController {
     private WorkOrderServiceImpl service;
 
     @GetMapping
-    private OperaResponse getAll(Integer orderId) {
+    private OperaResponse getAll(@RequestParam("orderId") Integer orderId) {
         OperaResponse response = new OperaResponse() ;
         response.setData(service.selectWorkOrderByOrderId(orderId));
         return response;
