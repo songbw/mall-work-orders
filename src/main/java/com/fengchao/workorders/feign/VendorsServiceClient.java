35cec1c7e6867fe214588c00a137398c32b36aae
song bingwei <bingwei.song@weesharing.com>
2020-10-10 11:55:07 +0800

    header

--- src/main/java/com/fengchao/workorders/feign/VendorsServiceClient.java
+++ src/main/java/com/fengchao/workorders/feign/VendorsServiceClient.java
@@ -3,10 +3,7 @@ package com.fengchao.workorders.feign;
 import com.fengchao.workorders.bean.OperaResponse;
 import com.fengchao.workorders.feign.hystric.VendorsServiceClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public interface VendorsServiceClient {
     OperaResponse<String> queryRenterId(@RequestParam("appId") String appId );
 
     @RequestMapping(value = "/renter/api/appIdList", method = RequestMethod.GET)
-    OperaResponse<List<String>> queryAppIdList(@RequestParam("renterId") String renterId);
+    OperaResponse<List<String>> queryAppIdList(@RequestHeader("renterId") String renterId);
 
     @RequestMapping(value = "/renter/api/companiesByAppId/{appId}", method = RequestMethod.GET)
     OperaResponse<List<Long>> queryAppIdMerchantList(@PathVariable("appId") String appId);
